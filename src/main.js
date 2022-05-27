5371a73e3adb37e68789484665d5dc2546fcc05a
bluej1996 <bluej1996@gamil.com>
2022-05-27 13:14:17 +0900

    최초발행

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import router from './router/index'
+// import store from './store/index'
+import './css/style.scss'
 
 createApp(App).mount('#app')
